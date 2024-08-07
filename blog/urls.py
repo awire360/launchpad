0db4c05e486f4ea6f124a3f24f130d39111fade7
awire360 <andy.warner@outlook.com>
2024-08-07 18:24:09 +0100

    update templates to bootstrap 5

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import index
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
-=======
-
-urlpatterns = [
-    
->>>>>>> 3ba9fba (update codebase to bootstrap 5)
 ]
--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", include("blog.urls")),
-=======
->>>>>>> 3ba9fba (update codebase to bootstrap 5)
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("__debug__", include("debug_toolbar.urls")),
